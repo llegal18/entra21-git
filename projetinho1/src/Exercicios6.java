3768861888dcb42fe5c4457e6db2c80b2f44aaaf
Larissa Ferreira de Souza Legal <llegal@furb.br>
2022-05-23 21:49:12 -0300

    POO

---
+++ projetinho1/src/Exercicios6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicios6 {
+    // POO
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+}
+
