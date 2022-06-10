af14df46904f7e4624f4b1c2b949e1baffb60e46
Larissa Ferreira de Souza Legal <llegal@furb.br>
2022-06-09 21:51:07 -0300

    Livraria

--- Livraria/src/Main.java
+++ Livraria/src/Main.java
@@ -1,15 +1,33 @@
+import classes.guardados.Estante;
+import classes.itens.Item;
 import classes.itens.Livro;
+import classes.*;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Livro l = new Livro();
+        Estante estante = new Estante(5);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Informe qual é o item da estante: (1 - Livro / 2 - DVD");
+            int itemE = in.nextInt();
+            System.out.print("Informe o título do item: ");
+            String estanteNome = in.nextLine();
+            System.out.print(" Informe o gênero do item: ");
+        }
+
+
+
+
+
+
+
+        /*Livro l = new Livro();
         l.setTitulo("Harry Potter e a Autora Transfóbica");
         l.avaliar();
         l.avaliar();
         l.avaliar();
-        System.out.println(l.getTotalRating());
+        System.out.println(l.getTotalRating());*/
     }
 }
--- Livraria/src/classes/guardados/Estante.java
+++ Livraria/src/classes/guardados/Estante.java
@@ -2,6 +2,8 @@ package classes.guardados;
 
 import classes.itens.Item;
 
+import java.util.Locale;
+
 public class Estante {
 
     private int capMaxima;
@@ -13,27 +15,42 @@ public class Estante {
     }
 
     public boolean estanteCheia() {
-        //TODO
-        return false;
+        return this.quantidadeItens() == this.getCapMaxima();
     }
+
     public int quantidadeItens() {
-        //TODO
-        return 0;
+        int contador = 0;
+        for (Item i : this.getItens()) {
+            if (i != null) {
+                contador++;
+            }
+        }
+        return contador;
     }
 
     public Item buscarItem(String titulo) {
-        //TODO
+        for (Item i : this.getItens()) {
+            if (i != null && i.getTitulo().toLowerCase().contains(titulo.toLowerCase())) {
+                return i;
+            }
+        }
         return null;
     }
 
     public boolean adicionarItem(Item item) {
-        //TODO
+        for (int i = 0; i < getItens().length; i++) {
+            if (this.getItens()[i] == null) {
+                this.getItens()[i] = item;
+                return true;
+            }
+        }
         return false;
     }
 
     public Item removerItem(int posicao) {
-        //TODO
-        return null;
+        Item i = this.getItens()[posicao];
+        this.getItens()[posicao] = null;
+        return i;
     }
 
     // GETTERS & SETTERS:
