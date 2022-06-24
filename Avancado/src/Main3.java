fe3a7e72b9e47ac422a8284db83f0d984ce5e3bd
Larissa Ferreira de Souza Legal <llegal@furb.br>
2022-06-24 18:37:17 -0300

    Carros

--- Avancado/src/Main3.java
+++ Avancado/src/Main3.java
@@ -3,13 +3,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main3 {
-    public static Scanner in = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
+    static HashMap<String, ArrayList<Carro>> carros = new HashMap<>();
 
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Carro>> carros = new HashMap<>();
-        while (true) {
+        boolean executa = true;
+        while (executa) {
             System.out.println("--------------------------------");
-            System.out.println("Menu:");
+            System.out.println("// Concessionária //:");
             System.out.println("1 - Ver catálogo");
             System.out.println("2 - Adicionar carro ao catálogo");
             System.out.println("3 - Vender");
@@ -18,21 +19,86 @@ public class Main3 {
             int escolha = in.nextInt();
             in.nextLine();
             switch (escolha) {
+                case 0:
+                    executa = false;
+                    break;
                 case 1:
-                    if (carros.isEmpty()) {
-                        System.out.println("Não há produtos cadastrados!");
-                    } else {
-                        System.out.println("Informe o produto: ");
-                        String busca = in.nextLine().toLowerCase();
-                        if (carros.containsKey(busca)) {
-                            System.out.println(busca + " : " + carros.get(busca));
-                        } else {
-                            System.out.println("Produto '" + busca + "' não encontrado!");
+                    mostrarListaCarros();
+                    break;
+                case 2:
+                    adicionarCarro();
+                    break;
+                case 3:
+                    venderCarro();
+                    break;
+                default:
+                    System.err.println("Selecione uma opção válida!");
+                    break;
+            }
+        }
+    }
+
+    public static void venderCarro() {
+        System.out.print("Informe a marca do carro: ");
+        String marca = in.nextLine();
+        if (!carros.containsKey(marca.toLowerCase())) {
+            System.out.println("Não existem carros da marca '" + marca + "'!");
+        } else {
+            System.out.print("Informe o modelo do carro: ");
+            String modelo = in.nextLine();
+            ArrayList<Carro> carrosMarca = carros.get(marca.toLowerCase());
+            boolean encontrado = false;
+            for (int i = 0; i < carrosMarca.size(); i++) {
+                Carro c = carrosMarca.get(i);
+                if (c.getModelo().equalsIgnoreCase(modelo)) {
+                    encontrado = true;
+                    System.out.printf("Valor da venda: R$%.2f. Deseja efetuar venda (S/N)? ", c.getValorVenda());
+                    if (in.nextLine().equalsIgnoreCase("S")) {
+                        System.out.printf("Carro %s da marca %s vendido por R$%.2f!\n", c.getModelo(), marca, c.getValorVenda());
+                        carrosMarca.remove(i);
+                        if (carrosMarca.isEmpty()) {
+                            carros.remove(marca.toLowerCase());
                         }
                     }
-
+                    break;
+                }
             }
-            break;
+            if (!encontrado) {
+                System.out.println("Carro de modelo '" + modelo + "' não encontrado na marca '" + marca + "'!");
+            }
         }
+        System.out.println();
+    }
+
+    public static void adicionarCarro() {
+        System.out.print("Informe a marca do carro: ");
+        String marca = in.nextLine().toLowerCase();
+        Carro c = new Carro();
+        System.out.print("Informe o modelo do carro: ");
+        c.setModelo(in.nextLine());
+        System.out.print("Informe o valor de venda do carro: R$");
+        c.setValorVenda(in.nextDouble());
+        in.nextLine();
+        if (!carros.containsKey(marca)) {
+            carros.put(marca, new ArrayList<>());
+        }
+        carros.get(marca).add(c);
+        System.out.println("Carro adicionado ao catálogo!");
+        System.out.println();
+    }
+
+    public static void mostrarListaCarros() {
+        if (carros.isEmpty()) {
+            System.out.println("Não existem carros cadastrados!");
+        } else {
+            System.out.println("** Catálogo **");
+            for (String key : carros.keySet()) {
+                System.out.println("--- " + key.toUpperCase() + " ---");
+                for (Carro c : carros.get(key)) {
+                    System.out.printf("%s: R$%.2f\n", c.getModelo(), c.getValorVenda());
+                }
+            }
+        }
+        System.out.println();
     }
 }
