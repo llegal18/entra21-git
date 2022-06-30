63446abbbe8d9d68298a7dbfd76a8f107bf0645e
Larissa Ferreira de Souza Legal <llegal@furb.br>
2022-06-30 18:45:54 -0300

    Enum

---
+++ Enum/src/Carta/Carta.java
@@ -0,0 +1,26 @@
+package Carta;
+
+public class Carta {
+
+    private Naipe naipe;
+
+    private ValorCarta valor;
+
+    //GETTERS & SETTERS:
+
+    public Naipe getNaipe() {
+        return naipe;
+    }
+
+    public void setNaipe(Naipe naipe) {
+        this.naipe = naipe;
+    }
+
+    public ValorCarta getValor() {
+        return valor;
+    }
+
+    public void setValor(ValorCarta valor) {
+        this.valor = valor;
+    }
+}
